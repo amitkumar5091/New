d96f5e19135923b67567fc2cfa535e89884892d7
Aaryan <159102426+aaryan235@users.noreply.github.com>
2024-06-06 22:37:02 +0530

    Update 00_4K.js

--- Nawaz/commands/00_4K.js
+++ Nawaz/commands/00_4K.js
@@ -1,37 +1,55 @@
 module.exports.config = {
-  name: "4k",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "NLam182",//lụm đc code ban đầu ko có cre nên chịu
-  description: "",
-  commandCategory: "tiện ích",
-  usages: "[reply]",
-  cooldowns: 0
+ name: "uid",
+ version: "1.0.0",
+ permission: 0,
+ credits: "Aaryan",
+ usePrefix: false,
+ description: "get user id.",
+ commandCategory: "without prefix",
+ cooldowns: 5
 };
 
-module.exports.run = async function({ api, event, args }) {
-  const fs = global.nodemodule["fs-extra"];
-  const axios = require('axios').default;
-  const isLink = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(args[0]);
-  var linkUp = event.messageReply.attachments[0].url || (isLink ? args[0] : '');
-  if (!linkUp) return api.sendMessage('Vui lòng reply 1 ảnh hoặc nhập link ảnh!', event.threadID, event.messageID);
-  try {
-    if (isLink) {
-      const response = await axios.get(linkUp, { responseType: "arraybuffer" });
-      api.sendMessage("Đang load, chờ tí nhé....!", event.threadID);
-      fs.writeFileSync(__dirname + `/cache/netanh.png`, Buffer.from(response.data, "binary"));
-    } else {
-      const res = await axios.get(`https://sumiproject.io.vn/imgur?link=${encodeURIComponent(linkUp)}&apikey=Free_1744646768`);
-      const link = res.data.uploaded.image;
-      const response = await axios.get(`https://sumiproject.io.vn/lamnet?link=${link}`, { responseType: "arraybuffer" });
-      api.sendMessage("Đang địt chờ bố 1 tí....!", event.threadID);
-      fs.writeFileSync(__dirname + `/cache/netanh.png`, Buffer.from(response.data, "binary"));
-    }
-    return api.sendMessage({
-      body: `🧸Ảnh của mày đây!`,
-      attachment: fs.createReadStream(__dirname + `/cache/netanh.png`)
-    }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/netanh.png`), event.messageID);
-  } catch (e) {
-    return api.sendMessage(e, event.threadID, event.messageID);
-  }
-};
+module.exports.run = async function({ event, api, args, client, Currencies, Users, utils, __GLOBAL, reminder }) {
+const fs = global.nodemodule["fs-extra"];
+		const request = global.nodemodule["request"];
+		const axios = global.nodemodule['axios']; 
+		if(event.type == "message_reply") { 
+			let name = await Users.getNameUser(event.messageReply.senderID) 
+ uid = event.messageReply.senderID
+ var callback = () =>   api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${uid}\n[ ▶️]➜ 𝗜𝗕: m.me/${uid}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${uid}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+		return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+				() => callback()); 
+		}
+		if (!args[0]) {
+			var uid = event.senderID;
+			const res = await axios.get(`https://www.nguyenmanh.name.vn/api/fbInfo?id=${uid}&apikey=LV7LWgAp`);
+var name = res.data.result.name 
+				var callback = () =>  api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${event.senderID}\n[ ▶️]➜ 𝗜𝗕: m.me/${event.senderID}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${event.senderID}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+		return request(encodeURI(`https://graph.facebook.com/${event.senderID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+				() => callback()); 
+		}
+		else {
+ if (args[0].indexOf(".com/")!==-1) {
+		const res_ID = await api.getUID(args[0]);
+	 var name = data.name
+var data = await api.getUserInfoV2(res_ID);
+		var username = data.username
+		var link = data.link
+		var callback = () => api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${res_ID}\n[ ▶️]➜ 𝗜𝗕: m.me/${res_ID}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: ${link}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+		return request(encodeURI(`https://graph.facebook.com/${res_ID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+				() => callback()); }
+ else {
+	if (args.join().indexOf('@') !== -1) 
+			var uid = Object.keys(event.mentions) 
+			var callback = () => 
+api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${uid}\n[ ▶️]➜ 𝗜𝗕: m.me/${uid}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${uid}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+		return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+				() => callback()); 
+
+ }
+}
+}
